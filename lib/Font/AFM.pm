1b8ae06a3759ddb689cbd6c5ac71cce074830757
David Warring david.warring@gmail.com <david.warring@gmail.com>
2015-05-21 13:10:07 -0700

    convert Font::AFM class to block form

--- lib/Font/AFM.pm
+++ lib/Font/AFM.pm
@@ -1,7 +1,7 @@
 # This -*- perl6 -*-  module is a simple parser for Adobe Font Metrics files.
 
 class Font::AFM:vers<1.20>
-    is Hash;
+    is Hash {
 
 =begin pod
 
@@ -182,219 +182,236 @@ it under the same terms as Perl itself.
 
 =end pod
 
-#-------perl 6 resumes here--------------------------------------------
+    #-------perl 6 resumes here--------------------------------------------
 
-method class-name($font-name) {
-    [~] "Font::Metrics::", $font-name.subst( /[:i'.afm'$]/, '').lc;
-}
+    method class-name($font-name) {
+        [~] "Font::Metrics::", $font-name.subst( /[:i'.afm'$]/, '').lc;
+    }
 
-method core-font($font-name) {
-    my $class-name = self.class-name($font-name);
-    require ::($class-name);
-    ::($class-name).new;
-}
+    method core-font($font-name) {
+        my $class-name = self.class-name($font-name);
+        require ::($class-name);
+        ::($class-name).new;
+    }
 
-# Creates a new Font::AFM object from an AFM file.  Pass it the name of the
-# font as parameter.
-# Synopisis:
-#
-#    $h = Font::AFM.new("Helvetica");
-#
+    # Creates a new Font::AFM object from an AFM file.  Pass it the name of the
+    # font as parameter.
+    # Synopisis:
+    #
+    #    $h = Font::AFM.new("Helvetica");
+    #
 
-multi submethod BUILD( Str :$name! is copy) {
+    multi submethod BUILD( Str :$name! is copy) {
 
-   my $metrics = {};
+       my $metrics = {};
 
-   $name ~~ s/'.afm' $//;
-   my $file;
+       $name ~~ s/'.afm' $//;
+       my $file;
 
-  if ~$*DISTRO ~~ m:i{^VMS} {
-       # Perl 5 porters note: Perl 6 on VMS?
-       $file = [~] 'sys$ps_font_metrics:', $name, '.afm';
-   } else {
-       $file = $name ~ '.afm';
-       unless $*SPEC.is-absolute($file) {
-           # not absolute, search the metrics path for the file
-           my @metrics_path = %*ENV<METRICS>:exists
-             ?? %*ENV<METRICS>.split(/\:/)>>.subst(rx{'/'$},'')
-             !! < /usr/lib/afm  /usr/local/lib/afm
-                  /usr/openwin/lib/fonts/afm  . >;
-
-	   for @metrics_path {
-               my $candidate = $*SPEC.catfile( $_, $file);
-	       if $candidate.IO ~~ :f {
-		   $file = $candidate;
-		   last;
-	       }
-	   }
-       }
-   }
-
-   die "Can't find the AFM file for $name ($file)"
-     unless $file.IO ~~ :e;
-
-   my $afm = $file.IO.open( :r );
-
-   for $afm.lines {
-
-       if /^StartKernData/   ff /^EndKernData/ {
-           next unless m:s/ <|w> KPX  $<glyph1>=['.'?\w+] $<glyph2>=['.'?\w+] $<kern>=[< + - >?\d+] /;
-           $metrics<KernData>{ $<glyph1> }{ $<glyph2> } = $<kern>.Int;
-       }
-       next if /^StartComposites/ ff /^EndComposites/; # same for composites
-       if /^StartCharMetrics/    ff /^EndCharMetrics/ {
-	   # only lines that start with "C" or "CH" are parsed
-	   next unless /^ C H? ' ' /;
-	   my $name  = ~ m:s/ <|w> N  <('.'?\w+)> ';' /;
-	   my $wx    = + m:s/ <|w> WX <(\d+)>     ';' /;
-           warn "no bbox: $_"
-	       unless m:s/ <|w> B [ (< + - >?\d+) ]+ ';' /;
-	   my $bbox = [ @0.map: { .Int } ];
-	   # Should also parse lingature data (format: L successor lignature)
-	   $metrics<Wx>{$name} = $wx;
-	   $metrics<BBox>{$name} = $bbox;
-	   next;
-       }
-
-       last if /^EndFontMetrics/;
-
-       if /(^\w+)' '+(.*)/ {
-           my $key = ~ $0;
-           my $val = ~ $1;
-           $metrics{$key} = $val;
+      if ~$*DISTRO ~~ m:i{^VMS} {
+           # Perl 5 porters note: Perl 6 on VMS?
+           $file = [~] 'sys$ps_font_metrics:', $name, '.afm';
        } else {
-	   die "Can't parse: $_";
+           $file = $name ~ '.afm';
+           unless $*SPEC.is-absolute($file) {
+               # not absolute, search the metrics path for the file
+               my @metrics_path = %*ENV<METRICS>:exists
+                 ?? %*ENV<METRICS>.split(/\:/)>>.subst(rx{'/'$},'')
+                 !! < /usr/lib/afm  /usr/local/lib/afm
+                      /usr/openwin/lib/fonts/afm  . >;
+
+               for @metrics_path {
+                   my $candidate = $*SPEC.catfile( $_, $file);
+                   if $candidate.IO ~~ :f {
+                       $file = $candidate;
+                       last;
+                   }
+               }
+           }
        }
-   }
 
-   $afm.close;
+       die "Can't find the AFM file for $name ($file)"
+         unless $file.IO ~~ :e;
 
-   unless $metrics<Wx><.notdef>:exists {
-       $metrics<Wx><.notdef> = 0;
-       $metrics<BBox><.notdef> = [ 0, 0, 0, 0];
-   }
+       my $afm = $file.IO.open( :r );
 
-   self.BUILD(:$metrics);
-}
+       for $afm.lines {
 
-multi submethod BUILD( Hash :$metrics! ) {
-    self{.key} = .value for $metrics.pairs;
-}
+           if /^StartKernData/   ff /^EndKernData/ {
+               next unless m:s/ <|w> KPX  $<glyph1>=['.'?\w+] $<glyph2>=['.'?\w+] $<kern>=[< + - >?\d+] /;
+               $metrics<KernData>{ $<glyph1> }{ $<glyph2> } = $<kern>.Int;
+           }
+           next if /^StartComposites/ ff /^EndComposites/; # same for composites
+           if /^StartCharMetrics/    ff /^EndCharMetrics/ {
+               # only lines that start with "C" or "CH" are parsed
+               next unless /^ CH? ' ' /;
+               my $name  = ~ m:s/ <|w> N  <('.'?\w+)> ';' /;
+               my $wx    = + m:s/ <|w> WX <(\d+)>     ';' /;
+               warn "no bbox: $_"
+                   unless m:s/ <|w> B [ (< + - >?\d+) ]+ ';' /;
+               my $bbox = [ @0.map: { .Int } ];
+               # Should also parse lingature data (format: L successor lignature)
+               $metrics<Wx>{$name} = $wx;
+               $metrics<BBox>{$name} = $bbox;
+               next;
+           }
 
-multi method new(Str $name)  { self.bless( :$name ) }
-multi method new(Hash $metrics) { self.bless( :$metrics ) }
+           last if /^EndFontMetrics/;
 
-BEGIN our @ISOLatin1Encoding = <
- .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef
- .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef
- .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef
- .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef space
- exclam quotedbl numbersign dollar percent ampersand quoteright
- parenleft parenright asterisk plus comma minus period slash zero one
- two three four five six seven eight nine colon semicolon less equal
- greater question at A B C D E F G H I J K L M N O P Q R S
- T U V W X Y Z bracketleft backslash bracketright asciicircum
- underscore quoteleft a b c d e f g h i j k l m n o p q r s
- t u v w x y z braceleft bar braceright asciitilde .notdef .notdef
- .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef
- .notdef .notdef .notdef .notdef .notdef .notdef .notdef dotlessi grave
- acute circumflex tilde macron breve dotaccent dieresis .notdef ring
- cedilla .notdef hungarumlaut ogonek caron space exclamdown cent
- sterling currency yen brokenbar section dieresis copyright ordfeminine
- guillemotleft logicalnot hyphen registered macron degree plusminus
- twosuperior threesuperior acute mu paragraph periodcentered cedilla
- onesuperior ordmasculine guillemotright onequarter onehalf threequarters
- questiondown Agrave Aacute Acircumflex Atilde Adieresis Aring AE
- Ccedilla Egrave Eacute Ecircumflex Edieresis Igrave Iacute Icircumflex
- Idieresis Eth Ntilde Ograve Oacute Ocircumflex Otilde Odieresis
- multiply Oslash Ugrave Uacute Ucircumflex Udieresis Yacute Thorn
- germandbls agrave aacute acircumflex atilde adieresis aring ae
- ccedilla egrave eacute ecircumflex edieresis igrave iacute icircumflex
- idieresis eth ntilde ograve oacute ocircumflex otilde odieresis divide
- oslash ugrave uacute ucircumflex udieresis yacute thorn ydieresis
->;
+           if /(^\w+)' '+(.*)/ {
+               my $key = ~ $0;
+               my $val = ~ $1;
+               $metrics{$key} = $val;
+           } else {
+               die "Can't parse: $_";
+           }
+       }
 
-# Returns an 256 element latin1 subset that maps from characters to width
-multi method wx-table($enc = 'latin1') {
-    self<_wx_table>{$enc} //= [
-        @ISOLatin1Encoding.map( -> $glyph-name {
-            self<Wx>{$glyph-name} // self<Wx><.notdef>;
-	} )
-    ];
-}
+       $afm.close;
 
-method stringwidth( Str $string, Numeric $pointsize?, Bool :$kern ) {
-    my $width = 0.0;
-    my $prev-glyph;
-    my $kern-data = self.KernData if $kern;
-    my $wx = self.wx-table('latin-1');
+       unless $metrics<Wx><.notdef>:exists {
+           $metrics<Wx><.notdef> = 0;
+           $metrics<BBox><.notdef> = [ 0, 0, 0, 0];
+       }
 
-    for $string.ords {
-	$width += $wx[$_] // 0;
-        my $glyph-name = @ISOLatin1Encoding[$_] // '.notdef';
-
-        $width += $kern-data{$prev-glyph}{$glyph-name}
-           if $kern && $prev-glyph && ($kern-data{$prev-glyph}{$glyph-name}:exists);
-
-        $prev-glyph = $glyph-name;
+       self.BUILD(:$metrics);
     }
-    if ($pointsize) {
-	$width *= $pointsize / 1000;
+
+    multi submethod BUILD( Hash :$metrics! ) {
+        self{.key} = .value for $metrics.pairs;
     }
-    $width;
-}
 
-#| kern a string. decompose into an array of: (['string', $width, $kern] , ... )
-method kern( Str $string, Numeric $pointsize? ) {
-    my $width = 0.0;
-    my $prev-glyph;
-    my $str = '';
-    my @kerns;
-    my $kern-data = self.KernData;
-    my $wx = self.wx-table('latin-1');
+    multi method new(Str $name)  { self.bless( :$name ) }
+    multi method new(Hash $metrics) { self.bless( :$metrics ) }
 
-    for $string.ords {
-        my $glyph-name = @ISOLatin1Encoding[$_] // '.notdef';
+    BEGIN our @ISOLatin1Encoding = <
+     .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef
+     .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef
+     .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef
+     .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef space
+     exclam quotedbl numbersign dollar percent ampersand quoteright
+     parenleft parenright asterisk plus comma minus period slash zero one
+     two three four five six seven eight nine colon semicolon less equal
+     greater question at A B C D E F G H I J K L M N O P Q R S
+     T U V W X Y Z bracketleft backslash bracketright asciicircum
+     underscore quoteleft a b c d e f g h i j k l m n o p q r s
+     t u v w x y z braceleft bar braceright asciitilde .notdef .notdef
+     .notdef .notdef .notdef .notdef .notdef .notdef .notdef .notdef
+     .notdef .notdef .notdef .notdef .notdef .notdef .notdef dotlessi grave
+     acute circumflex tilde macron breve dotaccent dieresis .notdef ring
+     cedilla .notdef hungarumlaut ogonek caron space exclamdown cent
+     sterling currency yen brokenbar section dieresis copyright ordfeminine
+     guillemotleft logicalnot hyphen registered macron degree plusminus
+     twosuperior threesuperior acute mu paragraph periodcentered cedilla
+     onesuperior ordmasculine guillemotright onequarter onehalf threequarters
+     questiondown Agrave Aacute Acircumflex Atilde Adieresis Aring AE
+     Ccedilla Egrave Eacute Ecircumflex Edieresis Igrave Iacute Icircumflex
+     Idieresis Eth Ntilde Ograve Oacute Ocircumflex Otilde Odieresis
+     multiply Oslash Ugrave Uacute Ucircumflex Udieresis Yacute Thorn
+     germandbls agrave aacute acircumflex atilde adieresis aring ae
+     ccedilla egrave eacute ecircumflex edieresis igrave iacute icircumflex
+     idieresis eth ntilde ograve oacute ocircumflex otilde odieresis divide
+     oslash ugrave uacute ucircumflex udieresis yacute thorn ydieresis
+    >;
 
-        if $prev-glyph && ($kern-data{$prev-glyph}{$glyph-name}:exists) {
-            my $kern = $kern-data{$prev-glyph}{$glyph-name};
-            if ($pointsize) {
-                $width *= $pointsize / 1000;
-                $kern *= $pointsize / 1000;
-            }
-            @kerns.push: [$str, $width, $kern ];
-            $str = '';
-            $width = 0.0;
+    # Returns an 256 element latin1 subset that maps from characters to width
+    multi method wx-table($enc = 'latin1') {
+        self<_wx_table>{$enc} //= [
+            @ISOLatin1Encoding.map( -> $glyph-name {
+                self<Wx>{$glyph-name} // self<Wx><.notdef>;
+            } )
+        ];
+    }
+
+    method stringwidth( Str $string, Numeric $pointsize?, Bool :$kern ) {
+        my $width = 0.0;
+        my $prev-glyph;
+        my $kern-data = self.KernData if $kern;
+        my $wx = self.wx-table('latin-1');
+
+        for $string.ords {
+            $width += $wx[$_] // 0;
+            my $glyph-name = @ISOLatin1Encoding[$_] // '.notdef';
+
+            $width += $kern-data{$prev-glyph}{$glyph-name}
+               if $kern && $prev-glyph && ($kern-data{$prev-glyph}{$glyph-name}:exists);
+
+            $prev-glyph = $glyph-name;
         }
-
-        $width += $wx[$_] // 0;
-        $str ~= .chr;
-        $prev-glyph = $glyph-name;
-    }
-
-    if $str.chars {
         if ($pointsize) {
             $width *= $pointsize / 1000;
         }
-        @kerns.push: [ $str, $width, 0]
+        $width;
     }
 
-    @kerns;
+    #| kern a string. decompose into an array of: (['string', $width, $kern] , ... )
+    proto method encode( Str $, Numeric $? ) {*}
+    multi method encode( Str $string, Numeric $pointsize?, Bool :$kern where $_ ) {
+        my $width = 0.0;
+        my $prev-glyph;
+        my $str = '';
+        my @chunks;
+        my $kern-data = self.KernData;
+        my $wx = self.wx-table('latin-1');
+
+        for $string.ords {
+            my $glyph-name = @ISOLatin1Encoding[$_] // next;
+
+            if $prev-glyph && ($kern-data{$prev-glyph}{$glyph-name}:exists) {
+                my $kerning = $kern-data{$prev-glyph}{$glyph-name};
+                if ($pointsize) {
+                    $width *= $pointsize / 1000;
+                    $kerning *= $pointsize / 1000;
+                }
+                @chunks.push: [ $str, $width, $kerning ];
+                $str = '';
+                $width = 0.0;
+            }
+
+            $width += $wx[$_] // 0;
+            $str ~= .chr;
+            $prev-glyph = $glyph-name;
+        }
+
+        if $str.chars {
+            if ($pointsize) {
+                $width *= $pointsize / 1000;
+            }
+            @chunks.push: [ $str, $width, 0]
+        }
+
+        @chunks;
+    }
+
+    multi method encode( Str $string, Numeric $pointsize?) is default {
+        my $width = 0.0;
+        my $str = '';
+        my $wx = self.wx-table('latin-1');
+
+        for $string.ords {
+            my $glyph-name = @ISOLatin1Encoding[$_] // next;
+            $width += $wx[$_] // 0;
+            $str ~= .chr;
+        }
+
+        $width *= $pointsize / 1000
+            if $pointsize;
+
+        [[ $str, $width, 0]].flat;
+    }
+
+    method !is-prop($prop-name) {
+        BEGIN constant KnownProps = set < FontName FullName FamilyName Weight
+        ItalicAngle IsFixedPitch FontBBox UnderlinePosition
+        UnderlineThickness Version Notice Comment EncodingScheme
+        CapHeight XHeight Ascender Descender Wx BBox KernData>;
+        $prop-name ∈ KnownProps;
+    }
+
+    multi method FALLBACK(Str $prop-name where self!"is-prop"($prop-name)) {
+        self.WHAT.^add_method($prop-name, { self{$prop-name} } );
+        self."$prop-name"();
+    }
+
+    multi method FALLBACK($name) is default { die "unknown method: $name\n" }
 }
-
-method !is-prop($prop-name) {
-    BEGIN constant KnownProps = set < FontName FullName FamilyName Weight
-    ItalicAngle IsFixedPitch FontBBox UnderlinePosition
-    UnderlineThickness Version Notice Comment EncodingScheme
-    CapHeight XHeight Ascender Descender Wx BBox KernData>;
-    $prop-name ∈ KnownProps;
-}
-
-multi method FALLBACK(Str $prop-name where self!"is-prop"($prop-name)) {
-    self.WHAT.^add_method($prop-name, { self{$prop-name} } );
-    self."$prop-name"();
-}
-
-multi method FALLBACK($name) is default { die "unknown method: $name\n" }
-
-
