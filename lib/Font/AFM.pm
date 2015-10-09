307af2d479d047944ef0b0f3ffde53a0b379e1b6
David Warring <david.warring@gmail.com>
2015-10-09 17:58:50 +0000

    adjust kerning .push for latest rakudo

--- lib/Font/AFM.pm
+++ lib/Font/AFM.pm
@@ -384,10 +384,10 @@ it under the same terms as Perl itself.
 
             if $prev-glyph && ($kern-data{$prev-glyph}{$glyph-name}:exists) {
                 my Numeric $kerning = $kern-data{$prev-glyph}{$glyph-name};
-                if ($pointsize) {
-                    $kerning *= $pointsize / 1000;
-                }
-                @chunks.push: ( $str, $kerning );
+                $kerning *= $pointsize / 1000
+                    if $pointsize;
+                @chunks.push: $str;
+                @chunks.push: $kerning;
                 $str = '';
             }
 
